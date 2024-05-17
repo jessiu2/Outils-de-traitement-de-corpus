5b7066289c549c9fddf38a875e236dfa1d417b8e
Jing LIU <59293302+jessiu2@users.noreply.github.com>
2024-05-18 00:08:59 +0200

    Create calcul_corpus.py

---
+++ projet/scripts/calcul_corpus.py
@@ -0,0 +1,23 @@
+import nltk
+from nltk.tokenize import word_tokenize
+from collections import Counter
+import sys
+
+def diversite_lexicale(texte):
+    tokens = word_tokenize(texte)
+    return len(set(tokens)) / len(tokens)
+
+def lire_corpus(file_path):
+    # Ouvrir et lire le fichier
+    with open(file_path, 'r', encoding='utf-8') as file:
+        texte_corpus = file.read()
+    return texte_corpus
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <path_to_corpus_file>")
+    else:
+        file_path = sys.argv[1]
+        texte_corpus = lire_corpus(file_path)
+        score_diversite = diversite_lexicale(texte_corpus)
+        print("Diversité lexicale :", score_diversite)
