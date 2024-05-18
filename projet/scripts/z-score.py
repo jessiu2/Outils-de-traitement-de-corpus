548e2eadcb8f902abb48d14a24ea5250618e10cc
Jing LIU <59293302+jessiu2@users.noreply.github.com>
2024-05-18 03:12:48 +0200

    Update z-score.py

--- projet/scripts/z-score.py
+++ projet/scripts/z-score.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 # Charger les données
-df = pd.read_csv('corpus_wiki_fr_cleaned.txt', delimiter='\t')
+df = pd.read_csv('/Outils-de-traitement-de-corpus/projet/data/processed/corpus_wiki_fr_cleaned.txt', delimiter='\t')
 
 # Calculer les z-scores
 from scipy import stats
@@ -16,4 +16,4 @@ outliers = (z_scores > threshold)
 df_cleaned = df[(outliers == False).all(axis=1)]
 
 # Sauvegarder les données nettoyées
-df_cleaned.to_csv('corpus_wiki_fr_cleaned_cleaned.txt', sep='\t', index=False)
+df_cleaned.to_csv('corpus_wiki_fr_cleaned_zscore.txt', sep='\t', index=False)
