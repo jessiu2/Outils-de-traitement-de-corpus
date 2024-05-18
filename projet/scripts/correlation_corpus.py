d38882f12a2d221efd4127e43909b0bbb7f419a1
Jing LIU <59293302+jessiu2@users.noreply.github.com>
2024-05-18 02:51:02 +0200

    Update correlation_corpus.py

--- projet/scripts/correlation_corpus.py
+++ projet/scripts/correlation_corpus.py
@@ -1,21 +1,35 @@
+import sys
 import numpy as np
 import pandas as pd
 from scipy.stats import pearsonr
 
-with open('~/Outils-de-traitement-de-corpus/projet/data/processed/corpus_wiki_fr_cleaned.txt', 'r', encoding='utf-8') as file:
-    text_data = file.readlines()
+def compute_correlation(file_path):
+    # Lire les données textuelles depuis le fichier
+    with open(file_path, 'r', encoding='utf-8') as file:
+        text_data = file.readlines()
 
-# Créer un DataFrame pour stocker la longueur de chaque texte et la fréquence du mot 'tourism'
-data = {'text_length': [], 'tourism_count': []}
+    # Créer un DataFrame pour stocker la longueur de chaque texte et la fréquence du mot 'tourism'
+    data = {'text_length': [], 'tourism_count': []}
 
-for text in text_data:
-    length = len(text)  # Longueur du texte
-    count = text.lower().count('tourism')  # Compter les occurrences du mot 'tourism'
-    data['text_length'].append(length)
-    data['tourism_count'].append(count)
+    for text in text_data:
+        length = len(text)  # Longueur du texte
+        count = text.lower().count('tourism')  # Compter les occurrences du mot 'tourism'
+        data['text_length'].append(length)
+        data['tourism_count'].append(count)
 
-df = pd.DataFrame(data)
+    df = pd.DataFrame(data)
 
-# Calculer la corrélation
-correlation, _ = pearsonr(df['text_length'], df['tourism_count'])
-print("Corrélation entre la longueur du texte et la fréquence de 'tourism' :", correlation)
+    # Calculer la corrélation
+    correlation, _ = pearsonr(df['text_length'], df['tourism_count'])
+    return correlation
+
+if __name__ == '__main__':
+    # Vérifier si le script a reçu suffisamment d'arguments
+    if len(sys.argv) < 2:
+        print("Utilisation : python correlation.py chemin-vers-fichier")
+        sys.exit(1)  # Quitter le script indiquant que l'entrée n'était pas correcte
+
+    file_path = sys.argv[1]
+    correlation = compute_correlation(file_path)
+    print("Corrélation entre la longueur du texte et la fréquence de 'tourism' :", correlation)
+
