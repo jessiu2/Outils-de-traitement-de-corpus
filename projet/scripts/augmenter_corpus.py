8691dcc73d7349731b60afdcacded0c9c18fe9d1
Jing LIU <59293302+jessiu2@users.noreply.github.com>
2024-05-18 03:15:56 +0200

    Update augmenter_corpus.py

--- projet/scripts/augmenter_corpus.py
+++ projet/scripts/augmenter_corpus.py
@@ -1,12 +1,10 @@
 import nlpaug.augmenter.word as naw
 import random
 import nltk
+import sys
+
 nltk.download('averaged_perceptron_tagger')
 nltk.download('wordnet')
-
-# Charger les données
-with open('/Outils-de-traitement-de-corpus/projet/data/processed/corpus_wiki_fr_cleaned.txt', 'r', encoding='utf-8') as file:
-    lines = file.readlines()
 
 # Fonction pour augmenter une ligne de texte
 def augment_text(text, aug, num_aug=1):
@@ -16,15 +14,30 @@ def augment_text(text, aug, num_aug=1):
         augmented_texts.append(augmented_text)
     return augmented_texts
 
-# Augmenteur utilisant des synonymes
-syn_aug = naw.SynonymAug(aug_src='wordnet')
+def main(input_file_path, output_file_path):
+    # Charger les données
+    with open(input_file_path, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
 
-# Augmenter les données
-augmented_lines = []
-for line in lines:
-    augmented_lines.extend(augment_text(line, syn_aug, num_aug=3))  # Augmenter chaque ligne 3 fois
+    # Augmenteur utilisant des synonymes
+    syn_aug = naw.SynonymAug(aug_src='wordnet')
 
-# Sauvegarder les données augmentées
-with open('/mnt/data/corpus_wiki_fr_cleaned_augmented.txt', 'w', encoding='utf-8') as file:
-    for line in augmented_lines:
-        file.write(line + "\n")
+    # Augmenter les données
+    augmented_lines = []
+    for line in lines:
+        augmented_lines.extend(augment_text(line, syn_aug, num_aug=3))  # Augmenter chaque ligne 3 fois
+
+    # Sauvegarder les données augmentées
+    with open(output_file_path, 'w', encoding='utf-8') as file:
+        for line in augmented_lines:
+            file.write(line + "\n")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python script.py <input_file_path> <output_file_path>")
+        sys.exit(1)
+    
+    input_file_path = sys.argv[1]
+    output_file_path = sys.argv[2]
+    
+    main(input_file_path, output_file_path)
