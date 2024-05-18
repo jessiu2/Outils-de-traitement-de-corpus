4126004a0f8385d07333d6b788f50178ff2735f3
Jing LIU <59293302+jessiu2@users.noreply.github.com>
2024-05-18 03:16:49 +0200

    Update z-score.py

--- projet/scripts/z-score.py
+++ projet/scripts/z-score.py
@@ -1,19 +1,31 @@
 import pandas as pd
 import numpy as np
-
-# Charger les données
-df = pd.read_csv('/Outils-de-traitement-de-corpus/projet/data/processed/corpus_wiki_fr_cleaned.txt', delimiter='\t')
-
-# Calculer les z-scores
+import sys
 from scipy import stats
-z_scores = np.abs(stats.zscore(df.select_dtypes(include=[np.number])))
 
-# Définir un seuil (par exemple, 3)
-threshold = 3
-outliers = (z_scores > threshold)
+def clean_data(input_file_path, output_file_path):
+    # Charger les données
+    df = pd.read_csv(input_file_path, delimiter='\t')
 
-# Supprimer les lignes avec des outliers
-df_cleaned = df[(outliers == False).all(axis=1)]
+    # Calculer les z-scores
+    z_scores = np.abs(stats.zscore(df.select_dtypes(include=[np.number])))
 
-# Sauvegarder les données nettoyées
-df_cleaned.to_csv('corpus_wiki_fr_cleaned_zscore.txt', sep='\t', index=False)
+    # Définir un seuil (par exemple, 3)
+    threshold = 3
+    outliers = (z_scores > threshold)
+
+    # Supprimer les lignes avec des outliers
+    df_cleaned = df[(outliers == False).all(axis=1)]
+
+    # Sauvegarder les données nettoyées
+    df_cleaned.to_csv(output_file_path, sep='\t', index=False)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python script.py <input_file_path> <output_file_path>")
+        sys.exit(1)
+    
+    input_file_path = sys.argv[1]
+    output_file_path = sys.argv[2]
+    
+    clean_data(input_file_path, output_file_path)
